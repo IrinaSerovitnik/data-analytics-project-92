753e0eb17ccb2aa2beb0a7377bc2d25e3cd97b6b
Irina Serovitnik <136731847+IrinaSerovitnik@users.noreply.github.com>
2024-03-21 02:15:25 +0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,5 +1,6 @@
 --считает общее количество покупателей из таблицы customers
 select COUNT(*) as customers_count from customers;
+
 --выводит десятку лучших продавцов по выручке с проданных товаров
 select
 CONCAT(e.first_name, ' ', e.last_name) as seller,
@@ -13,6 +14,7 @@ on e.employee_id = s.sales_person_id
 group by CONCAT(e.first_name, ' ', e.last_name)
 order by SUM(p.price*s.quantity) desc
 limit 10;
+
 --выводит продавцов, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам
 with sellers as (
 select
@@ -31,3 +33,16 @@ average_income
 from sellers
 where average_income < (select AVG(average_income) from sellers)
 order by average_income;
+
+--выводит информацию о выручке по дням недели
+select
+CONCAT(e.first_name, ' ', e.last_name) as seller,
+TO_CHAR(s.sale_date, 'Day') as day_of_week,
+FLOOR(SUM(p.price*s.quantity)) as income
+from sales as s
+left join products as p
+on s.product_id = p.product_id
+left join employees as e
+on e.employee_id = s.sales_person_id
+group by extract(ISODOW FROM s.sale_date), day_of_week, seller
+order by extract(ISODOW FROM s.sale_date), seller;
