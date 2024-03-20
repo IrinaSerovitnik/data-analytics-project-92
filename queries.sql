6a796781c8877a05eab795243b8294adef1b6356
Irina Serovitnik <136731847+IrinaSerovitnik@users.noreply.github.com>
2024-03-20 23:45:13 +0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -14,3 +14,22 @@ on e.employee_id = s.sales_person_id
 group by CONCAT(e.first_name, ' ', e.last_name)
 order by SUM(p.price*s.quantity) desc
 limit 10;
+
+--выводит продавцов, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам
+with sellers as (
+select
+CONCAT(e.first_name, ' ', e.last_name) as seller,
+FLOOR(AVG(p.price*s.quantity)) as average_income
+from sales as s
+left join products as p
+on s.product_id = p.product_id
+left join employees as e
+on e.employee_id = s.sales_person_id
+group by CONCAT(e.first_name, ' ', e.last_name)
+)
+select
+seller,
+average_income
+from sellers
+where average_income < (select AVG(average_income) from sellers)
+order by average_income;
